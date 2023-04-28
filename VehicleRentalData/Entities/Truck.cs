2e6e1da04c231a5394aea3fbcea9a92640df7a8a
ghiza <ghizasanmatei15@gmail.com>
2023-04-28 20:18:46 +0300

    Project Setup2 (fixed warnings)

--- VehicleRentalData/Entities/Truck.cs
+++ VehicleRentalData/Entities/Truck.cs
@@ -13,7 +13,7 @@ namespace VehicleRentalData.Entities
         public int Id { get; set; }
         public int EnginePower { get; set; }
         public int TransportCapacity { get; set; }
-        public string RequiredDriverLicense { get; set; }
+        public string? RequiredDriverLicense { get; set; }
         public int FuelConsumption { get; set; }
         public int Mileage { get; set; }
         public bool HasTrailer { get; set; }
