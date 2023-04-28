58b5c3b11a644a02380020d8f12b7ccb2b9f441e
ghiza <ghizasanmatei15@gmail.com>
2023-04-28 20:05:28 +0300

    Add project files.

---
+++ VehicleRentalData/Entities/Truck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRentalData.Entities
+{
+    public class Truck
+    {
+        [Key]
+        public int Id { get; set; }
+        public int EnginePower { get; set; }
+        public int TransportCapacity { get; set; }
+        public string RequiredDriverLicense { get; set; }
+        public int FuelConsumption { get; set; }
+        public int Mileage { get; set; }
+        public bool HasTrailer { get; set; }
+    }
+}
