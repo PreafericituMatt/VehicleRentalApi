bd89bd576e4697bcc46d211a550d16aa2815f278
ghiza <ghizasanmatei15@gmail.com>
2023-04-28 20:21:22 +0300

    First Migration and added data service to programCs

--- VehicleRentalData/DataContext.cs
+++ VehicleRentalData/DataContext.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VehicleRentalData.Entities;
 
 namespace VehicleRentalData
 {
-    public class DbContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Truck> Trucks { get; set; }
     }
 }
---
+++ VehicleRentalData/Migrations/20230428172055_InitialMigration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehicleRentalData.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EnginePower = table.Column<int>(type: "int", nullable: false),
+                    TransportCapacity = table.Column<int>(type: "int", nullable: false),
+                    RequiredDriverLicense = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FuelConsumption = table.Column<int>(type: "int", nullable: false),
+                    Mileage = table.Column<int>(type: "int", nullable: false),
+                    NumberOfSeats = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trucks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EnginePower = table.Column<int>(type: "int", nullable: false),
+                    TransportCapacity = table.Column<int>(type: "int", nullable: false),
+                    RequiredDriverLicense = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FuelConsumption = table.Column<int>(type: "int", nullable: false),
+                    Mileage = table.Column<int>(type: "int", nullable: false),
+                    HasTrailer = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trucks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "Trucks");
+        }
+    }
+}
